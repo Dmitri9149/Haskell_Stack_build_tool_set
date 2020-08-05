86b1713e160af779713e9724b958d96ccf051ec7
dmitri9149 <dmitri.as.name@gmail.com>
2020-08-05 17:49:51 +0300

    isAllDigits fun is added

--- src/AddStr.hs
+++ src/AddStr.hs
@@ -1,6 +1,7 @@
-module Lib
-    ( someFunc
-    ) where
+module AddStr where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Data.Char
+
+isAllDigits :: String -> Bool
+isAllDigits val = all (== True)
+  $ map (\x -> x `elem` ['1', '2', '3', '4', '5', '6', '7', '8', '9']) val
