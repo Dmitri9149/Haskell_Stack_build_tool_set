7dcf49e101e2b75f450d30b46290c5423ce708ce
dmitri9149 <dmitri.as.name@gmail.com>
2020-08-06 17:59:32 +0300

    bugs re fixed after runnuig tests

--- src/AddStr.hs
+++ src/AddStr.hs
@@ -3,5 +3,5 @@ module AddStr where
 import           Data.Char
 
 isAllDigits :: String -> Bool
-isAllDigits val = all (== True)
-  $ map (\x -> x `elem` ['1', '2', '3', '4', '5', '6', '7', '8', '9']) val
+isAllDigits "" == False
+isAllDigits val = all (== True) $ map isDigit val
