8b704fc8c6105c6a4a3600f123d104e18cdf3b57
dmitri9149 <dmitri.as.name@gmail.com>
2020-08-06 18:25:18 +0300

    addition of 2 strings functionality

--- src/AddStr.hs
+++ src/AddStr.hs
@@ -5,3 +5,10 @@ import           Data.Char
 isAllDigits :: String -> Bool
 isAllDigits "" = False
 isAllDigits val = all (== True) $ map isDigit val
+
+strAddInts :: String -> String -> Either String Int
+strAddInts a b
+  | isAllDigits a && isAllDigits b       = Right (read a + read b)
+  | not (isAllDigits a || isAllDigits b) = Left "Both args are wrong"
+  | not (isAllDigits a)                  = Left "First arg is wrong"
+  | otherwise                            = Left "Second ars is wrong"
