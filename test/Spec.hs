344d9cdeaec508fef41d3b1296a0ad204c8e8a5b
dmitri9149 <dmitri.as.name@gmail.com>
2020-08-06 15:17:28 +0300

    some tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+import           AddStr
+
+main :: IO ()
+main = do
+  -- quickCheck prop_isAllDigit
+  putStrLn ""
+  putStrLn $ if isAllDigits "123" then "OK" else "FAIL!"
+  putStrLn $ if not $ isAllDigits "4st" then "OK" else "FAIL!"
+  putStrLn $ if not $ isAllDigits "123_4" then "OK" else "FAIL!"
+  return ()
