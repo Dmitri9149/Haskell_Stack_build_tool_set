d6a11e7e529c56b33ee44ee0c39625b98b94b145
dmitri9149 <dmitri.as.name@gmail.com>
2020-08-06 17:50:38 +0300

    QuickCheck used

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,15 @@
 import           AddStr
+import           Data.Char
+import           Test.QuickCheck
+
+prop_isAllDigit :: String -> Bool
+prop_isAllDigit val = if isAllDigits val || val == ""
+  then onlyDigit == length val
+  else onlyDigit /= length val
+  where onlyDigit = length $ filter isDigit val
 
 main :: IO ()
 main = do
-  -- quickCheck prop_isAllDigit
-  putStrLn ""
-  putStrLn $ if isAllDigits "123" then "OK" else "FAIL!"
-  putStrLn $ if not $ isAllDigits "4st" then "OK" else "FAIL!"
-  putStrLn $ if not $ isAllDigits "123_4" then "OK" else "FAIL!"
-  return ()
+  quickCheck prop_isAllDigit
+  putStrLn "Done"
+
